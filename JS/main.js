b9c5da8cf973349c1a36974a23c7177b4f6bdabd
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-11 09:28:09 -0300

    separando o código em mais arquivos para melhor organização

--- JS/main.js
+++ JS/main.js
@@ -9,23 +9,3 @@ async function buscarLivrosAPI() {
 }
 
 buscarLivrosAPI();
-
-const inserirLivrosIndex = document.querySelector("#livros");
-
-function mostrarLivros(arrayLivros) {
-  arrayLivros.forEach((livro) => {
-    inserirLivrosIndex.innerHTML += `
-    <div class="livro">
-    <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-    <h2 class="livro__titulo">
-    ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco}</p>
-      <div class="tags">
-      <span class="tag">${livro.categoria}</span>
-      </div>
-      </div>
-      `;
-  });
-}
