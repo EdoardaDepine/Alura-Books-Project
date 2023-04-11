f8a00d37d26ecdd147abd8672b26cb919a885019
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-11 14:27:46 -0300

    utilzando o método Reduce para exibir o vlor total apenas dos livros disponíveis

--- JS/metodoFilter.js
+++ JS/metodoFilter.js
@@ -4,8 +4,23 @@ botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
 function filtrarLivros() {
   const botao = document.getElementById(this.id);
   const categoriaBotao = botao.value;
-  const livrosFiltrados = livros.filter(
-    (livro) => livro.categoria == categoriaBotao
-  );
+  const livrosFiltrados =
+    categoriaBotao == "disponivel"
+      ? livros.filter((livro) => livro.quantidade > 0)
+      : livros.filter((livro) => livro.categoria == categoriaBotao);
   mostrarLivros(livrosFiltrados);
+  if (categoriaBotao == "disponivel") {
+    const valorTotal = valorTotalLivrosDisponiveisNaTela(livrosFiltrados);
+    exibirValorTotalLivrosDisponiveis(valorTotal);
+  }
+}
+
+function exibirValorTotalLivrosDisponiveis(valorTotal) {
+  valorTotalLivrosDisponiveis.innerHTML = `
+  <div class='livros__disponiveis'>
+  <p>
+    Todos os livros disponíveis por R$ <span id='valor'>${valorTotal}</span>
+  </p>
+</div>;
+  `;
 }
--- JS/metodoForEach.js
+++ JS/metodoForEach.js
@@ -1,11 +1,17 @@
 const inserirLivrosIndex = document.querySelector("#livros");
+const valorTotalLivrosDisponiveis = document.querySelector(
+  "#valor_total_livros_disponiveis"
+);
 
 function mostrarLivros(arrayLivros) {
   inserirLivrosIndex.innerHTML = "";
+  valorTotalLivrosDisponiveis.innerHTML = "";
   arrayLivros.forEach((livro) => {
+    let disponibilidade =
+      livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
     inserirLivrosIndex.innerHTML += `
     <div class="livro">
-    <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+    <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
     <h2 class="livro__titulo">
     ${livro.titulo}
       </h2>
